5864b67a0c525ee0a2ee72b7689b4e30d0a4cf52
fashionmm <fashionmm@126.com>
2016-05-05 17:34:04 +0800

    首页设计

--- Presentation/ECommerce.Web/App_Start/BundleConfig.cs
+++ Presentation/ECommerce.Web/App_Start/BundleConfig.cs
@@ -21,10 +21,12 @@ namespace ECommerce.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                       "~/Scripts/swiper.jquery.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                       "~/Content/swiper/swiper.css",
                       "~/Content/site.css"));
         }
     }
